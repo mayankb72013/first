dea2357a5dcc999a276e846fa68b6378b6bab9be
Mayank Balmoori <balmoorimayank@gmail.com>
2024-10-25 14:17:31 +0530

    Initial code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const age = 24;
+const names = 'mayank';
+const dob = '13/06/2005';
+
+function print(){
+    console.log(`${names}`);
+}
+function msg(){
+    console.log(`This is just another message`);
+    
+}
